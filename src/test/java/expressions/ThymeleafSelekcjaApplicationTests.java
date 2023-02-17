c7ed695896748ce65cf050d9a1e7576e323da388
User <user@its.me>
2023-02-17 14:34:56 +0100

    Initial commit

---
+++ src/test/java/expressions/ThymeleafSelekcjaApplicationTests.java
@@ -0,0 +1,13 @@
+package expressions.thymeleafselekcja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSelekcjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
