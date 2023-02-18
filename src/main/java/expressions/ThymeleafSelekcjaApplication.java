6ae0271455839cfb01055657af899bed36a260fe
User <user@its.me>
2023-02-18 11:04:23 +0100

    selection - simplier calling variable

--- src/main/java/expressions/ThymeleafSelekcjaApplication.java
+++ src/main/java/expressions/ThymeleafSelekcjaApplication.java
@@ -1,4 +1,4 @@
-package expressions.thymeleafselekcja;
+package expressions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/expressions/ThymeleafSelekcjaApplicationTests.java
+++ src/test/java/expressions/ThymeleafSelekcjaApplicationTests.java
@@ -1,4 +1,4 @@
-package expressions.thymeleafselekcja;
+package expressions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
